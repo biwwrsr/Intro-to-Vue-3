16c524c0a0558763792710f1b79361e5dc12b0c8
biwwrsr <6206021620132@fitm.kmutnb.ac.th>
2021-04-07 17:19:01 +0700

    modify

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
-            premium: true
+            cart: [],
+            premium: false
         }
     },
-    methods: {}
+    methods: {
+        updateCart(id){
+            this.cart.push(id)
+        }
+    }
 })
